c63cc01214e6dd06035331ad6e1bc94013cad5d5
hkdnet <hkdnet@users.noreply.github.com>
2017-09-17 10:08:41 +0900

    Add #show

--- lib/ideadiff/cli.rb
+++ lib/ideadiff/cli.rb
@@ -6,7 +6,14 @@ module Ideadiff
 
     # @return [Fixnum] status
     def exec
+      text = show('HEAD~', 'Gemfile')
+      return 1 unless $?.success?
+      puts text
       0
+    end
+
+    def show(rev, file)
+      `git show #{rev}:#{file}`
     end
   end
 end
