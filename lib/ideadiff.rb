58d559a82fbb5198120828f42f2126fa8e292a4f
hkdnet <hkdnet@users.noreply.github.com>
2017-09-17 09:55:01 +0900

    Add exe

--- lib/ideadiff.rb
+++ lib/ideadiff.rb
@@ -1,4 +1,5 @@
 require "ideadiff/version"
+require "ideadiff/cli"
 
 module Ideadiff
   # Your code goes here...
---
+++ lib/ideadiff/cli.rb
@@ -0,0 +1,12 @@
+module Ideadiff
+  class Cli
+    def initialize(args)
+      @args = args
+    end
+
+    # @return [Fixnum] status
+    def exec
+      0
+    end
+  end
+end
